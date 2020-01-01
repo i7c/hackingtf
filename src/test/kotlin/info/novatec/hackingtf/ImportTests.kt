5e3b2f446163aa9dc4e1589516bf04ecb16fadcc
Constantin Weißer <constantin.weisser@novatec-gmbh.de>
2020-01-01 19:14:41 +0100

    Get terraform plan

--- src/test/kotlin/info/novatec/hackingtf/ImportTests.kt
+++ src/test/kotlin/info/novatec/hackingtf/ImportTests.kt
@@ -20,16 +20,22 @@ class ImportTests {
         setupImportDir(workingDir)
         val tfSchema = getTfSchema(workingDir)
         stateImport(workingDir, importResources)
-
-//
-//            val tfPlanFile = File(it, "plan")
-//            val tfPlan = bashCaptureJson { """
-//                terraform plan -no-color -out "${tfPlanFile.absolutePath}" &> /dev/null
-//                terraform show -json "${tfPlanFile.absolutePath}"
-//            """.trimIndent() } as Map<String, Any?>
+        val tfPlan = getTfPlan(workingDir)
 
         println(workingDir.absolutePath)
         workingDir.deleteRecursively()
+    }
+
+    private fun getTfPlan(workingDir: File): Map<String, Any?> {
+        return cd(workingDir) {
+            val tfPlanFile = File(it, "plan")
+            bashCaptureJson {
+                """
+                    terraform plan -no-color -out "${tfPlanFile.absolutePath}" &> /dev/null
+                    terraform show -json "${tfPlanFile.absolutePath}"
+                """.trimIndent()
+            } as Map<String, Any?>
+        }
     }
 
     private fun stateImport(
@@ -53,7 +59,9 @@ class ImportTests {
     private fun mapToTfType(id: String, name: String): String =
         when {
             Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.DBforMySQL/servers/.*$").matches(id) -> "azurerm_mysql_server.$name"
-            Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.ContainerService/managedClusters/.*$").matches(id) -> "azurerm_kubernetes_cluster.$name"
+            Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.ContainerService/managedClusters/.*$").matches(
+                id
+            ) -> "azurerm_kubernetes_cluster.$name"
             Regex("/subscriptions/.*/resourceGroups/.*/providers/Microsoft.Network/virtualNetworks/.*$").matches(id) -> "azurerm_virtual_network.$name"
             else -> throw IllegalArgumentException("Unknown resource type $id")
         }

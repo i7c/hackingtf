b48a08dd6bb771498d1f64651b3aa92f768e5443
Constantin Weißer <constantin.weisser@novatec-gmbh.de>
2020-01-01 20:00:13 +0100

    Move useful functions to dedicated file to clean up the code

--- src/test/kotlin/info/novatec/hackingtf/ImportTests.kt
+++ src/test/kotlin/info/novatec/hackingtf/ImportTests.kt
@@ -1,6 +1,5 @@
 package info.novatec.hackingtf
 
-import java.io.File
 import org.junit.Test
 
 class ImportTests {
@@ -25,71 +24,4 @@ class ImportTests {
         println(workingDir.absolutePath)
         workingDir.deleteRecursively()
     }
-
-    private fun getTfPlan(workingDir: File): Map<String, Any?> {
-        return cd(workingDir) {
-            val tfPlanFile = File(it, "plan")
-            bashCaptureJson {
-                """
-                    terraform plan -no-color -out "${tfPlanFile.absolutePath}" &> /dev/null
-                    terraform show -json "${tfPlanFile.absolutePath}"
-                """.trimIndent()
-            } as Map<String, Any?>
-        }
-    }
-
-    private fun stateImport(
-        workingDir: File,
-        importResources: List<Map<String, Any?>>
-    ) {
-        cd(workingDir) {
-            importResources.forEach {
-                bash {
-                    """
-                        terraform import -allow-missing-config "${mapToTfType(
-                        it["id"] as String,
-                        it["name"] as String
-                    )}" "${it["id"]}"
-                    """.trimIndent()
-                }
-            }
-        }
-    }
-
-    private fun mapToTfType(id: String, name: String): String =
-        when {
-            Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.DBforMySQL/servers/.*$").matches(id) -> "azurerm_mysql_server.$name"
-            Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.ContainerService/managedClusters/.*$").matches(id) -> "azurerm_kubernetes_cluster.$name"
-            Regex("/subscriptions/.*/resourceGroups/.*/providers/Microsoft.Network/virtualNetworks/.*$").matches(id) -> "azurerm_virtual_network.$name"
-            Regex("/subscriptions/.*/resourceGroups/.*$").matches(id) -> "azurerm_resource_group.$name"
-            else -> throw IllegalArgumentException("Unknown resource type $id")
-        }
-
-    private fun setupImportDir(workingDir: File) {
-        cd(workingDir) {
-            file(File(it, "main.tf")) {
-                """
-                    terraform {
-                      required_providers {
-                        azurerm = "~>1.29"
-                      }
-                    }
-                    
-                    provider azurerm {}
-                    """.trimIndent()
-            }
-
-            bash {
-                """
-                    terraform init -no-color
-                    terraform validate -no-color
-                """.trimIndent()
-            }
-        }
-    }
-
-    private fun getTfSchema(workingDir: File): Map<String, Any?> =
-        cd(workingDir) {
-            bashCaptureJson { "terraform providers schema -json" } as Map<String, Any?>
-        }
 }

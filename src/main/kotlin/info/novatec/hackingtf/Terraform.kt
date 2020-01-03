d085af86090d6b527b0bf459a7c97dff3d943142
Constantin Weißer <constantin.weisser@novatec-gmbh.de>
2020-01-03 11:32:45 +0100

    Move mapping function to Azure package

--- src/main/kotlin/info/novatec/hackingtf/Terraform.kt
+++ src/main/kotlin/info/novatec/hackingtf/Terraform.kt
@@ -1,5 +1,6 @@
 package info.novatec.hackingtf
 
+import info.novatec.hackingtf.azure.mapToTfType
 import java.io.File
 
 fun setupImportDir(workingDir: File) {
@@ -44,12 +45,3 @@ fun getTfPlan(workingDir: File): Map<String, Any?> {
 
 fun getTfSchema(workingDir: File): Map<String, Any?> =
     cd(workingDir) { bashCaptureJson { "terraform providers schema -json" } as Map<String, Any?> }
-
-fun mapToTfType(id: String, name: String): String =
-    when {
-        Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.DBforMySQL/servers/.*$").matches(id) -> "azurerm_mysql_server.$name"
-        Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.ContainerService/managedClusters/.*$").matches(id) -> "azurerm_kubernetes_cluster.$name"
-        Regex("/subscriptions/.*/resourceGroups/.*/providers/Microsoft.Network/virtualNetworks/.*$").matches(id) -> "azurerm_virtual_network.$name"
-        Regex("/subscriptions/.*/resourceGroups/.*$").matches(id) -> "azurerm_resource_group.$name"
-        else -> throw IllegalArgumentException("Unknown resource type $id")
-    }
---
+++ src/main/kotlin/info/novatec/hackingtf/azure/Azure.kt
@@ -0,0 +1,10 @@
+package info.novatec.hackingtf.azure
+
+fun mapToTfType(id: String, name: String): String =
+    when {
+        Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.DBforMySQL/servers/.*$").matches(id) -> "azurerm_mysql_server.$name"
+        Regex("^/subscriptions/.*/resourceGroups/.*/providers/Microsoft.ContainerService/managedClusters/.*$").matches(id) -> "azurerm_kubernetes_cluster.$name"
+        Regex("/subscriptions/.*/resourceGroups/.*/providers/Microsoft.Network/virtualNetworks/.*$").matches(id) -> "azurerm_virtual_network.$name"
+        Regex("/subscriptions/.*/resourceGroups/.*$").matches(id) -> "azurerm_resource_group.$name"
+        else -> throw IllegalArgumentException("Unknown resource type $id")
+    }

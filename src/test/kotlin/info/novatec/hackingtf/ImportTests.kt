ab55f1343b23620ad07058a34ccf4c55e0cf11f4
Constantin Weißer <constantin.weisser@novatec-gmbh.de>
2020-01-06 13:50:19 +0100

    Split import into two test methods for lower runtimes while testing

--- src/test/kotlin/info/novatec/hackingtf/ImportTests.kt
+++ src/test/kotlin/info/novatec/hackingtf/ImportTests.kt
@@ -2,40 +2,40 @@ package info.novatec.hackingtf
 
 import info.novatec.hackingtf.azure.azureIdToNamedResource
 import info.novatec.hackingtf.terraform.GenericConfigurationBlock
-import java.io.File
 import org.junit.Test
+import java.io.File
 
 class ImportTests {
 
-    @Test
-    fun `Terraform Import Test`() {
+    val workingDir = File("/tmp/stateimport").apply { mkdirs() }
 
+    @Test
+    fun `Import resources into statefile`() {
         val importRgs =
             bashCaptureJson {
-                """
-                az group list \
-                    | jq '[ .[] | select (.name | test("^cwe-importt?est")) ]'
-            """
+                """az group list | jq '[ .[] | select (.name | test("^cwe-importt?est")) ]'"""
             } as List<Map<String, Any?>>
+
         val importResources = importRgs.flatMap {
-            bashCaptureJson { """ az resource list -g "${it["name"]}" """ } as List<Map<String, Any?>>
+            bashCaptureJson { """az resource list -g "${it["name"]}"""" } as List<Map<String, Any?>>
         }.plus(importRgs)
 
-        val workingDir = createTempDir()
-
         tfInitNew(workingDir, "azurerm")
-        val resourceSchemas = getTfSchema(workingDir).jsonPath<Map<String, Any?>>("$.provider_schemas.azurerm.resource_schemas")
-
         cd(workingDir) {
             importResources.forEach {
                 tfImportResource(azureIdToNamedResource(it["id"] as String, it["name"] as String), it["id"] as String)
             }
         }
+    }
 
+
+    @Test
+    fun `Generate code`() {
+        val resourceSchemas = getTfSchema(workingDir).jsonPath<Map<String, Any?>>("$.provider_schemas.azurerm.resource_schemas")
         val resourceChanges = getTfPlan(workingDir).jsonPath<List<Map<String, Any?>>>("$.resource_changes")
 
         file(File(workingDir, "generated.tf")) {
-            resourceChanges.joinToString(separator = "\n\n") {
+            resourceChanges.joinToString(separator = "\n") {
                 GenericConfigurationBlock(
                     it["name"] as String,
                     it.jsonPath("$.change.before"),
@@ -45,13 +45,6 @@ class ImportTests {
             }
         }
 
-        cd(workingDir) { bash { """
-            set -e
-            terraform fmt
-            terraform validate
-        """.trimIndent() } }
-
-        println(workingDir.absolutePath)
-        workingDir.deleteRecursively()
+        cd(workingDir) { bash { "terraform fmt" } }
     }
 }
